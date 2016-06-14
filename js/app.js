5a6fecb8cc77ab380d766b09f83d8358fbade4a1
Adam Gautsch <adam@orangecoat.com>
2016-06-14 16:14:49 -0400

    Add files via upload
    JS Files Added

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  var city_slide = function() {
+	  $("#city").css("background-position", "center 0px");
+	};
+
+  setTimeout(city_slide, 1000); // wait a second, and transition the background in
+
+});
